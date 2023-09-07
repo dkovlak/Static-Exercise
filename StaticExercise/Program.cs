7bc2238551b23ca72f025b81250e6be519dd43d1
David Kovlak <kovlakdavid4@gmail.com>
2023-09-07 12:47:26 -0700

    Static

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("\n----------Fahtenheit To Celsius----------");
+            Console.WriteLine(TempConverter.FahtenheitToCelsius(32));
+
+            Console.WriteLine("\n----------Celsius To Fahrenheit----------");
+            Console.WriteLine($"{TempConverter.CelsiusToFahrenheit(0)}");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace StaticExercise
+{
+	public class TempConverter
+	{
+		public TempConverter()
+		{
+		}
+
+		public static double FahtenheitToCelsius(double num)
+			{
+			double result = (num - 32) * 5.0 / 9.0;
+			Console.WriteLine($"{num}°F = {result}°C");
+			return result;
+			}
+
+		public static double CelsiusToFahrenheit(double num)
+		{
+			double result = (num * 9.0 / 5.0) + 32;
+            Console.WriteLine($"{num}°C = {result}°F");
+            return result;
+		}
+	}
+}
+
